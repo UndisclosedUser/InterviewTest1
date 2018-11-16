d24c8bf113aba8420ac65f1d8580cff033e83172
David Rosenstiehl <david.rosenstiehl@aot.swiss>
2018-11-16 10:14:33 +0100

    parse words

--- test.cc
+++ test.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 int main(int argc, char const *argv[])
 {
@@ -7,6 +8,7 @@ int main(int argc, char const *argv[])
   if (argc != 2)
     {
       std::cout << "Usage : test text_file" << std::endl;
+      exit(1);
     }
 
   char const * input_filename = argv[1];
@@ -14,7 +16,24 @@ int main(int argc, char const *argv[])
   if (!input_file.is_open())
     {
       std::cout << "Could not open file : \"" << input_filename << "\"" << std::endl;
-      exit(1);
+      exit(2);
     }
-}
 
+  std::string line;
+  std::regex re("[a-zA-Z0-9\\.]+");
+  std::smatch match;
+  while (getline(input_file, line))
+    {
+      //std::cout << line << std::endl;
+      while (std::regex_search(line, match, re))
+	{
+	  for (auto x:match)
+	    {
+	      std::cout << x << std::endl;
+	    }
+	  line = match.suffix().str();
+	}
+    }
+
+  input_file.close();
+}
